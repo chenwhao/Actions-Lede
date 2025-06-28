a43fae59fe6ae4ee0790785cc97e1af02c225711
chenwhao <whaochan@gmail.com>
2025-06-28 23:15:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) ChenWenhao 2025
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# https://github.com/smallprogram/OpenWrtAction
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+cat << 'EOF' | cat - feeds.conf.default > temp && mv temp feeds.conf.default
+src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
+src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
+src-git helloworld https://github.com/fw876/helloworld;master
+src-git OpenClash https://github.com/vernesong/OpenClash;master
+src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main
+EOF
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+rm -rf ./package/custom_packages
+mkdir -p ./package/custom_packages
+
+git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/custom_packages/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/custom_packages/luci-app-argon-config
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 ./package/custom_packages/mosdns
+git clone https://github.com/sirpdboy/luci-app-netspeedtest ./package/custom_packages/netspeedtest
